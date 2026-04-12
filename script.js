619b669b35b00a84420f7e1f9c2b5306b4591a42
Younes Fernandez Chavero <fernandezyounesdev@gmail.com>
2026-04-12 20:57:37 +0200

    page d'accueil html & css complète

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const menuBtn = document.getElementById('menuBtn')
 const dropdown = document.getElementById('dropdown')
-menuBtn.addEventListener('click', function(e) { 
+
+menuBtn.addEventListener('click', function (e) {
     e.stopPropagation()
     dropdown.classList.toggle('open')
 })
-document.addEventListener('click', function() {
+
+document.addEventListener('click', function () {
     dropdown.classList.remove('open')
 })
