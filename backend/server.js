407f1df8687063b5378b5a32bdc58eb5ca383ace
Younes Fernandez Chavero <fernandezyounesdev@gmail.com>
2026-04-15 14:03:14 +0200

    création du backend

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+
+const PORT = 3000;
+
+app.get("/api/test", (req, res) => {
+    res.json({ message: "Backend OK ✔" });
+});
+
+app.get("/api/food", async (req, res) => {
+    const query = req.query.q;
+
+    const url = `https://api.spoonacular.com/food/ingredients/search?query=${query}&apiKey=${process.env.API_KEY}`;
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        res.json(data);
+
+    } catch (err) {
+        res.status(500).json({ error: "Erreur API" });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Serveur lancé sur http://localhost:${PORT}`);
+});
--- script.js
+++ script.js
@@ -20,3 +20,40 @@ dayCards.forEach(function(card) {
         card.classList.add('active')
     })
 })
+const API_KEY = "TA_CLE_API_ICI"; // Spoonacular
+
+async function searchFood() {
+    const query = document.getElementById("searchInput").value;
+
+    if (!query) return;
+
+    try {
+        const response = await fetch(
+            `https://api.spoonacular.com/food/ingredients/search?query=${query}&apiKey=${API_KEY}`
+        );
+
+        const data = await response.json();
+
+        displayResults(data.results);
+
+    } catch (error) {
+        console.error("Erreur API :", error);
+    }
+}
+
+function displayResults(results) {
+    const container = document.getElementById("results");
+    container.innerHTML = "";
+
+    results.forEach(item => {
+        container.innerHTML += `
+            <div class="exo-card">
+                <div class="exo-visual">🍎</div>
+                <div class="exo-body">
+                    <div class="exo-name">${item.name}</div>
+                    <div class="exo-desc">Aliment trouvé via API nutrition</div>
+                </div>
+            </div>
+        `;
+    });
+}
