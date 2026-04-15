d8cbfa94b8250e2d9fdc9a276c7562bdfc60fcfc
Younes Fernandez Chavero <fernandezyounesdev@gmail.com>
2026-04-15 12:18:59 +0200

    création page entrainement

--- script.js
+++ script.js
@@ -9,3 +9,14 @@ menuBtn.addEventListener('click', function (e) {
 document.addEventListener('click', function () {
     dropdown.classList.remove('open')
 })
+
+const dayCards = document.querySelectorAll('.day-card')
+
+dayCards.forEach(function(card) {
+    card.addEventListener('click', function() {
+        dayCards.forEach(function(c) {
+            c.classList.remove('active')
+        })
+        card.classList.add('active')
+    })
+})
